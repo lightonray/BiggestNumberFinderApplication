9cf6a79fafc72ae365f0c4bf3bc032985af9cab0
American Computers <American Computers@LIGHTONRAY>
2021-11-19 21:20:14 +0200

    Add project files.

---
+++ BiggestNumberFinderApplication/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BiggestNumberFinderApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.Write("Input the 1st number: ");
+                double num1 = double.Parse(Console.ReadLine());
+                
+                Console.Write("Input the 2nd number: ");
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.Write("Input the 3rd number: ");
+                double num3 = double.Parse(Console.ReadLine());
+
+                Console.Write("Input the 4rd number: ");
+                double num4 = double.Parse(Console.ReadLine());
+
+                if (num1 > num2 & num1 > num3 & num1 > num4)
+                {
+                    Console.WriteLine("The biggest number is: " + num1);
+                }
+                else if (num2 > num1 & num2 > num3 & num2 > num4)
+                {
+                    Console.WriteLine("The biggest number is: " + num2);
+                }
+                else if (num3 > num1 & num3 > num2 & num3 > num4)
+                {
+                    Console.WriteLine("The biggest number is: " + num3);
+                }
+                else
+                {
+                    Console.WriteLine("The biggest number is: " + num4);
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Please enter a valid input value");
+            }
+
+        }
+    }
+}
